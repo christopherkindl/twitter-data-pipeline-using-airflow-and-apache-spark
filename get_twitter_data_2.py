3cbc2a1fa6fe6db7c00105375c4f68f69cd8d766
Christopher Kindl <christopherkindl@Christopher-Kindls-MacBook-Pro.local>
2021-04-14 15:36:29 +0100

    add create schema and save to postgres function

--- get_twitter_data_2.py
+++ get_twitter_data_2.py
@@ -33,11 +33,11 @@ default_args = {
     'email_on_retry': False,
     'aws_conn_id': 'aws_default_christopherkindl',
     'bucket_name': Variable.get('london-housing-webapp', deserialize_json=True)['bucket_name'],
-    'postgres_conn_id': 'engineering_groupwork_carina', #change with your credentials
+    #'postgres_conn_id': 'engineering_groupwork_carina',
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'output_key': Variable.get('twitter_results',deserialize_json=True)['output_key'],
-    'db_name': Variable.get("housing_db", deserialize_json=True)['db_name']
+    'output_key': Variable.get('twitter_results',deserialize_json=True)['output_key']
+    #'db_name': Variable.get("housing_db", deserialize_json=True)['db_name']
 }
 
 dag = DAG('london-housing-webapp',
@@ -46,28 +46,6 @@ dag = DAG('london-housing-webapp',
           catchup=False,
           default_args=default_args,
           max_active_runs=1)
-
-
-# Creating schema if inexistant
-def create_schema(**kwargs):
-    pg_hook = PostgresHook(postgres_conn_id=kwargs['postgres_conn_id'], schema=kwargs['db_name'])
-    conn = pg_hook.get_conn()
-    cursor = conn.cursor()
-    log.info('Initialised connection') #change column types to float
-    sql_queries = """
-
-    CREATE SCHEMA IF NOT EXISTS london_schema;
-    DROP TABLE IF EXISTS london_schema.stations;
-    CREATE TABLE IF NOT EXISTS london_schema.stations(
-        "station" varchar(256),
-        "latitude" varchar(256),
-        "longitude" varchar(256)
-    );
-    """
-
-    cursor.execute(sql_queries)
-    conn.commit()
-    log.info("Created Schema and Table")
 
 
 def get_flat_file_station_information(**kwargs):
@@ -167,20 +145,63 @@ def get_flat_file_station_information(**kwargs):
 
     #df_total = df
 
-    # #convert df into dict
-    #data_dict = stations.to_dict('series')
-    #log.info('dict created')
+    #convert df into dict
+    data_dict = stations.to_dict('series')
+    log.info('dict created')
+
+    # #Establishing S3 connection
+    # s3 = S3Hook(kwargs['aws_conn_id'])
+    # key = Variable.get('twitter_output', deserialize_json=True)['output_key']
+    # bucket_name = kwargs['bucket_name']
+    #
+    # # Prepare the file to send to s3
+    # csv_buffer = io.StringIO()
+    #
+    # #Ensuring the CSV files treats "NAN" as null values
+    # data_dict_csv= stations.to_csv(csv_buffer, index=False)
+    #
+    # # Save the pandas dataframe as a csv to s3
+    # s3 = s3.get_resource_type('s3')
+    #
+    # # Get the data type object from pandas dataframe, key and connection object to s3 bucket
+    # data = csv_buffer.getvalue()
+    #
+    #
+    # object = s3.Object(bucket_name, key)
+    #
+    # # Write the file to S3 bucket in specific path defined in key
+    # object.put(Body=data)
+    #
+    # log.info('Finished saving the scraped data to s3')
+
+
+    return data_dict
+
+
+def save_to_s3(**kwargs):
+
 
     #Establishing S3 connection
     s3 = S3Hook(kwargs['aws_conn_id'])
     key = Variable.get('twitter_output', deserialize_json=True)['output_key']
     bucket_name = kwargs['bucket_name']
 
+    # Get the task instance
+    #task_instance = kwargs['ti']
+
+    # Get the output of the previous task
+    collected_data = task_instance.xcom_pull(
+        task_ids='get_flat_file_station_information')
+
+    log.info('xcom from collecting data task:{0}'.format(
+        collected_data))
+
+    # create dataframe for collected data
+    df = pd.DataFrame(collected_data)
+
     # Prepare the file to send to s3
     csv_buffer = io.StringIO()
-
-    #Ensuring the CSV files treats "NAN" as null values
-    data_dict_csv= stations.to_csv(csv_buffer, index=False)
+    df.to_csv(csv_buffer, index=False)
 
     # Save the pandas dataframe as a csv to s3
     s3 = s3.get_resource_type('s3')
@@ -188,102 +209,15 @@ def get_flat_file_station_information(**kwargs):
     # Get the data type object from pandas dataframe, key and connection object to s3 bucket
     data = csv_buffer.getvalue()
 
-
+    print("Saving CSV file")
     object = s3.Object(bucket_name, key)
 
     # Write the file to S3 bucket in specific path defined in key
     object.put(Body=data)
 
-    log.info('Finished saving the scraped data to s3')
+    log.info('Finished saving the scraped tweets to s3')
 
 
-    return
-
-
-# def save_to_s3(**kwargs):
-#
-#
-#     #Establishing S3 connection
-#     s3 = S3Hook(kwargs['aws_conn_id'])
-#     key = kwargs['output_key']
-#     bucket_name = kwargs['bucket_name']
-#
-#     # Get the task instance
-#     task_instance = kwargs['ti']
-#
-#     # Get the output of the previous task
-#     collected_data = task_instance.xcom_pull(
-#         task_ids='get_flat_file_station_information')
-#
-#     log.info('xcom from collecting data task:{0}'.format(
-#         collected_data))
-#
-#     # create dataframe for collected data
-#     df = pd.DataFrame(collected_data)
-#
-#     # Prepare the file to send to s3
-#     csv_buffer = io.StringIO()
-#     df.to_csv(csv_buffer, index=False)
-#
-#     # Save the pandas dataframe as a csv to s3
-#     s3 = s3.get_resource_type('s3')
-#
-#     # Get the data type object from pandas dataframe, key and connection object to s3 bucket
-#     data = csv_buffer.getvalue()
-#
-#     print("Saving CSV file")
-#     object = s3.Object(bucket_name, key)
-#
-#     # Write the file to S3 bucket in specific path defined in key
-#     object.put(Body=data)
-#
-#     log.info('Finished saving the scraped tweets to s3')
-
-# Saving Dexter file to postgreSQL database
-def save_result_to_postgres_db(**kwargs):
-
-    #Establishing connection to S3 bucket
-    bucket_name = kwargs['bucket_name']
-    key = Variable.get('twitter_output', deserialize_json=True)['output_key']
-    s3 = S3Hook(kwargs['aws_conn_id'])
-    log.info("Established connection to S3 bucket")
-
-
-    # Get the task instance
-    task_instance = kwargs['ti']
-    print(task_instance)
-
-
-    # Read the content of the key from the bucket
-    csv_bytes = s3.read_key(key, bucket_name)
-    # Read the CSV
-    stations = pd.read_csv(io.StringIO(csv_bytes ))#, encoding='utf-8')
-
-    log.info('passing data from S3 bucket')
-
-    # Connect to the PostgreSQL database
-    pg_hook = PostgresHook(postgres_conn_id=kwargs['postgres_conn_id'], schema=kwargs['db_name'])
-    conn = pg_hook.get_conn()
-    cursor = conn.cursor()
-
-    log.info('Initialised connection')
-
-    log.info('Loading row by row into database')
-
-    #Load the rows into the PostgresSQL database
-    s = """INSERT INTO london_schema.stations(station, latitude, longitude) VALUES (%s, %s, %s)"""
-
-    for index in range(len(stations)):
-        obj = []
-
-        obj.append([stations.Station[index],
-                   stations.Latitude[index],
-                   stations.Longitude[index]])
-
-        cursor.executemany(s, obj)
-        conn.commit()
-
-    log.info('Finished saving the data to postgres database')
 
 # def lambda_preprocessing(**kwargs):
 #     hook = AwsLambdaHook('twitterpreprocessing',
@@ -300,14 +234,6 @@ def save_result_to_postgres_db(**kwargs):
 # 3. Set up the main configurations of the dag
 # =============================================================================
 
-create_schema = PythonOperator(
-    task_id='create_schema',
-    python_callable=create_schema,
-    op_kwargs=default_args,
-    provide_context=True,
-    dag=dag,
-)
-
 get_flat_file_station_information = PythonOperator(
     task_id='get_flat_file_station_information',
     python_callable=get_flat_file_station_information,
@@ -317,23 +243,13 @@ get_flat_file_station_information = PythonOperator(
     dag=dag,
 )
 
-save_result_to_postgres_db = PythonOperator(
-    task_id='save_result_to_postgres_db',
+save_to_s3 = PythonOperator(
+    task_id='save_to_s3',
     provide_context=True,
-    python_callable=save_result_to_postgres_db,
-    trigger_rule=TriggerRule.ALL_SUCCESS,
+    python_callable=save_to_s3,
     op_kwargs=default_args,
     dag=dag,
-
 )
-
-# save_to_s3 = PythonOperator(
-#     task_id='save_to_s3',
-#     provide_context=True,
-#     python_callable=save_to_s3,
-#     op_kwargs=default_args,
-#     dag=dag,
-# )
 
 # lambda_preprocessing = PythonOperator(
 #     task_id='lambda_preprocessing',
@@ -348,7 +264,7 @@ save_result_to_postgres_db = PythonOperator(
 # 4. Indicating the order of the dags
 # =============================================================================
 
-create_schema >> get_flat_file_station_information >> save_result_to_postgres_db
+get_flat_file_station_information >> save_to_s3
 
 #For Alternative method
 # create_schema >> web_scraping_task_dexters >> s3_save_file_func
